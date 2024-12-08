ea95ca5927e585f770639f993bdc0f8628c9733e
tarun <tarunmalviya320@gmail.com>
2024-12-08 22:25:39 +0530

    backgrond music added

--- js/index.js
+++ js/index.js
@@ -1,35 +1,44 @@
-(function() {
+(function () {
   function $(id) {
     return document.getElementById(id);
   }
 
-  var card = $('card'),
-      openB = $('open'),
-      closeB = $('close'),
-      timer = null;
-  console.log('wat', card);
-  openB.addEventListener('click', function () {
-    const id = prompt('Enter my instagram userid to unlock:');
-    const realID = 'fascinator_tarun';
-    if(id===realID){
-    card.setAttribute('class', 'open-half');
-    if (timer) clearTimeout(timer);
-    timer = setTimeout(function () {
-      card.setAttribute('class', 'open-fully');
-      timer = null;
-    }, 1000);
-  }else{
-    alert('wrong! try again');
-  }
+  var card = $("card"),
+    openB = $("open"),
+    closeB = $("close"),
+    timer = null;
+
+  console.log("wat", card);
+
+  openB.addEventListener("click", function () {
+    const id = prompt("Enter my instagram userid to unlock:");
+    const realID = "fascinator_tarun";
+    if (id === realID) {
+      card.setAttribute("class", "open-half");
+
+      const backgroundMusic = document.getElementById('background-music');
+
+      backgroundMusic.loop = true;
+      backgroundMusic.play().catch((error) => {
+        console.error("Music playback failed:", error);
+      });
+
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(function () {
+        card.setAttribute("class", "open-fully");
+        timer = null;
+      }, 1000);
+    } else {
+      alert("wrong! try again");
+    }
   });
 
-  closeB.addEventListener('click', function () {
-    card.setAttribute('class', 'close-half');
+  closeB.addEventListener("click", function () {
+    card.setAttribute("class", "close-half");
     if (timer) clearTimerout(timer);
     timer = setTimeout(function () {
-      card.setAttribute('class', '');
+      card.setAttribute("class", "");
       timer = null;
     }, 1000);
   });
-
-}());
+})();
